0e482c446da972b1f5a12c292f9ac99839a82bd0
nileshjaiswal01 <nileshjaiswal2055@gmail.com>
2026-07-03 15:36:01 +0530

    V1

---
+++ python.py
@@ -0,0 +1,8 @@
+print("hello world !")
+a = int(input("Enter the value : "))
+b = int(input("Enter the value :"))
+c = a + b
+print(a)
+print(b)
+print(c)
+print(f"value of A is {a} value of B is {b} value of C is {c}")
